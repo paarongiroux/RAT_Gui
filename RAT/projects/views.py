f6eac3b0654dd3f3950293d9d4756ca794823be5
Aaron <pag87@nau.edu>
2020-03-13 10:56:09 -0700

    adds random floor map db update for proof of concept

--- RAT/projects/views.py
+++ RAT/projects/views.py
@@ -2,16 +2,23 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from projects.forms import LocationForm
 from projects.models import *
+import random
 
 
 class ProjectView(TemplateView):
 	template_name = "project_index.html"
 
 	def get(self, request):
+		# temp code to show retreival of projects field from database
+		random.seed()
+		randfloor = random.randint(1, 3)
 		proj = Project.objects.get(id=1)
 		proj.xLocation = proj.xLocation + 1
+		proj.floorNo = randfloor
 		proj.save()
 		projects = Project.objects.all()
+		# end temp code
+
 		form = LocationForm()
 		room = ""
 		floor = ""
@@ -24,6 +31,7 @@ class ProjectView(TemplateView):
 		return render(request, self.template_name, context)
 
 	def post(self, request):
+
 		form = LocationForm(request.POST)
 		if form.is_valid():
 			projects = Project.objects.all()
